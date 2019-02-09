a0de175c1dbcbf3a05f0d191f6999d8f92ab4cd9
rebecca-zhang <rebeccazhangnz@outlook.com>
2019-02-09 18:28:12 +1300

    updated basic routes and views

--- Server/routes/serverRoutes.js
+++ Server/routes/serverRoutes.js
@@ -6,5 +6,31 @@ router.get('/', (req, res) => {
   res.render('index')
 })
 
+router.get('/userHome/:user',(req,res)=>{
+  const userName = req.params.user
+  res.render('itinerary',{user: userName})
+})
+
+router.post('/userHome/:user',(req,res)=>{
+  console.log('added')
+})
+
+router.get('/signin', (req,res)=>{
+  res.render('signin')
+})
+
+router.post('/signin', (req,res)=>{
+  res.send('this is signin')
+})
+
+
+router.get('/signup', (req,res)=>{
+  res.render('signup')
+})
+
+
+router.post('/signin', (req,res)=>{
+  res.send('this is signup')
+})
 
 module.exports = router
