921e130195ebf39c0d9ed5506e30ca6f22cfca0b
rebecca-zhang <rebeccazhangnz@outlook.com>
2019-02-10 09:13:49 +1300

    basic view and route mvp

--- Server/routes/serverRoutes.js
+++ Server/routes/serverRoutes.js
@@ -6,30 +6,38 @@ router.get('/', (req, res) => {
   res.render('index')
 })
 
-router.get('/userHome/:user',(req,res)=>{
-  const userName = req.params.user
-  res.render('itinerary',{user: userName})
+router.get('/userHome/:user', (req, res) => {
+  let userName = req.params.user
+  let data = {
+    user: userName,
+    title: 'testing Trip',
+    email: `${userName}@travelpod.com`,
+    info: [
+      { date: '20190304', location: 'Auckland', event: 'SkyTower' },
+      { date: '20190305', location: 'Rotorua', event: 'Zipline' },
+      { date: '20190306', location: 'Taupo', event: 'Huka Falls' }
+    ]
+  }
+  res.render('itinerary', data)
 })
 
-router.post('/userHome/:user',(req,res)=>{
+router.post('/userHome/:user', (req, res) => {
   console.log('added')
 })
 
-router.get('/signin', (req,res)=>{
+router.get('/signin', (req, res) => {
   res.render('signin')
 })
 
-router.post('/signin', (req,res)=>{
+router.post('/signin', (req, res) => {
   res.send('this is signin')
 })
 
-
-router.get('/signup', (req,res)=>{
+router.get('/signup', (req, res) => {
   res.render('signup')
 })
 
-
-router.post('/signin', (req,res)=>{
+router.post('/signin', (req, res) => {
   res.send('this is signup')
 })
 
